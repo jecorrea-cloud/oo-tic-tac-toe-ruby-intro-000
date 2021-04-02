419467bac3f819f11b02672c30b26e0a12a6cf2b
Joaquin Correa <jecorrea@protonmail.com>
2021-04-02 02:21:21 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -38,4 +38,17 @@ class TicTacToe
   def valid_move?(board = @board, index)
     index.between?(0,8) && !position_taken?(board, index)
   end
+  
+  def turn_count(board)
+    counter = 0
+    board.each do |turn|
+      if turn == "X" || turn == "O"
+        counter +=1
+      end
+    end
+    counter
+  end
+  
+  
+  
 end
