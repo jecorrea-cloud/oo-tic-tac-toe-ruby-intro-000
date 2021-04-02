a47df9872d415afed27ec50a0b93de226659e08c
Joaquin Correa <jecorrea@protonmail.com>
2021-04-02 02:27:42 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -28,8 +28,7 @@ class TicTacToe
   end
 
   def move(index, current_player="X")
-    @board = board
-    board[index] = current_player
+    @board[index] = current_player
   end
 
   def position_taken?(board = @board, location)
