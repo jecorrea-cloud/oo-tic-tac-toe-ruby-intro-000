895b1cb477ca256e94a6560ad8bb9c2ab0f1405a
Joaquin Correa <jecorrea@protonmail.com>
2021-04-02 02:42:43 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -100,4 +100,17 @@ class TicTacToe
     end
   end
 
+  def play(board=@board)
+
+  until over?(board)
+    turn(board)
+  end
+  if won?(board)
+    winner(board) == "X" ||  winner(board) == "O"
+    puts "Congratulations #{winner(board)}!"
+  else draw?(board)
+    puts "Cat's Game!"
+  end
+end
+
 end
