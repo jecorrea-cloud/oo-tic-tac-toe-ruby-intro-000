ec9ffd0fe27bcdd28587ac5bbeea10506bc5ad03
Joaquin Correa <jecorrea@protonmail.com>
2021-04-02 02:24:15 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -53,7 +53,7 @@ class TicTacToe
     turn_count(board) % 2 == 0 ? "X" : "O"
   end
 
-  def turn(board)
+  def turn(board=@board)
     puts "Please enter 1-9:"
     user_input = gets.strip
     index = input_to_index(user_input)
